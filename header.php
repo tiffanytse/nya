c868ec656e0162acb9c9244547bea155dee4ec11
Tiffany Tse <ttse@coldfrontlabs.ca>
2014-01-24 01:01:13 -0500

    Added styles for social menu

--- header.php
+++ header.php
@@ -37,7 +37,7 @@
 	<header id="masthead" class="site-header cf" role="banner">
       <div class="wrapper">
         <hgroup>
-          <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><h1 class="site-title"><?php bloginfo( 'name' ); ?></h1></a>
         </hgroup>
 
         <nav id="site-navigation" class="main-navigation" role="navigation">
