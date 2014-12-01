7e6a38cb2b280b68c5534d717759dfcb9a9f7253
Tiffany Tse <ttse@coldfrontlabs.ca>
2014-11-30 22:09:56 -0500

    Add styling and js to stop sidebar TOC from going below footer

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,13 @@ jQuery(function($) {
     offset: 32 // Apply "stuck" when element 30px from top
   });
 
+  $('.footer').waypoint({
+    offset: $('.sticky-wrapper').height()+64, //calculate menu's height and margin before footer
+    handler: function(direction) {
+      $('.toc-container').toggleClass('sticky-bottom'); //change position to absolute for the wrapper
+    }
+  });
+
+
 });
 
