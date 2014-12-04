a65b7742f1c3f45ba992b409dd020a46c14e172c
Tiffany Tse <ttse@coldfrontlabs.ca>
2014-12-04 11:10:19 -0500

    Add styling for donate link, update font sizing

--- header.php
+++ header.php
@@ -25,6 +25,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <link href="<?php echo get_stylesheet_directory_uri(); ?>/flexslider.css" rel="stylesheet">
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" /> 
 <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
